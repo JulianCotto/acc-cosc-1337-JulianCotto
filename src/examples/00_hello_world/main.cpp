ed5d9f1f8170470fcf4fc52c956803ed20f24885
juliancotto <julian.cotto@g.austincc.edu>
2022-08-24 19:02:05 -0500

    8-24 Hello World program w/ notes

--- src/examples/00_hello_world/main.cpp
+++ src/examples/00_hello_world/main.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+// ^--- pre-processor > include input (cin) & output (cout) stream
+
+using std::cout;
+// ^--- from the standard library > use cout (input)
+// ^--- better practice than "using namespace std;"
 
 int main()
+// ^--- every program MUST-HAVE a value returning main()
+
 {
-    std::cout<<"Hello World!";
+// "{" <--- indicates the start of a function
+
+    cout << "Hello World!";
+    // ^--- console out (print) "Hello World"
+
     return 0;
+    // ^--- value returned from main()
+
 }
+// "}" <--- indicates the end of a function
